a4b861b273ec905d0e3ad08ed99ba41a11b70e30
joyxiaogu <1475607055@qq.com>
2020-10-24 10:03:44 +0800

    init a project

---
+++ src/main/java/com/example/demogit/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demogit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "Welcome to here.";
+    }
+
+}
