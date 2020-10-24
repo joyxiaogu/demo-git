211ab5d1ed973b9281d59ee6d57b9209b7d2fd5b
joyxiaogu <1475607055@qq.com>
2020-10-24 12:46:28 +0800

    feature2 branch

--- src/main/java/com/example/demogit/controller/HelloController.java
+++ src/main/java/com/example/demogit/controller/HelloController.java
@@ -10,7 +10,8 @@ public class HelloController {
     @RequestMapping("/index")
     public String index(){
         return "Welcome to here!" +
-                "update by idea.. Merging  code and resolving problems. V0.9 通过rebase合并（合并后分支不在标准日志上展示了）";
+                "update by idea.. Merging  code and resolving problems. V0.9 通过rebase合并（合并后分支不在标准日志上展示了）" +
+                "feature2...";
 
     }
 
