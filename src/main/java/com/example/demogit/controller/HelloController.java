747d893f1eaed3c4c848702ef7d9fa7a98a413c2
joyxiaogu <1475607055@qq.com>
2020-10-24 12:04:04 +0800

    rebase合并分支

--- src/main/java/com/example/demogit/controller/HelloController.java
+++ src/main/java/com/example/demogit/controller/HelloController.java
@@ -10,8 +10,7 @@ public class HelloController {
     @RequestMapping("/index")
     public String index(){
         return "Welcome to here!" +
-                "update by idea.. Merging  code and resolving problems. V0.86" +
-                "add one by feature (really)" ;
+                "update by idea.. Merging  code and resolving problems. V0.9 通过rebase合并（合并后分支不在标准日志上展示了）";
     }
 
 }
